b6aea2e4b9a2ab04724638d032cf2ce8a12941ea
Elis Antonio Perez <codewithleader@gmail.com>
2025-03-14 13:55:16 -0500

    feat: :sparkles: enhance CreateVenueDto with S3Object validation and optional images array

--- src/venues/dto/create-venue.dto.ts
+++ src/venues/dto/create-venue.dto.ts
@@ -1,4 +1,30 @@
-import { IsInt, IsString, IsUUID } from 'class-validator';
+import {
+  IsInt,
+  IsString,
+  IsUUID,
+  ValidateNested,
+  IsArray,
+  IsOptional,
+  Validate,
+} from 'class-validator';
+import { S3Object } from 'src/venues/interfaces/venue.interface';
+
+class S3ObjectClass {
+  @IsString()
+  bucketName: string;
+
+  @IsString()
+  displayName: string;
+
+  @IsString()
+  key: string;
+
+  @IsString()
+  type: string;
+
+  @IsString()
+  url: string;
+}
 
 export class CreateVenueDto {
   @IsString({ message: 'Name must be a string' })
@@ -9,4 +35,18 @@ export class CreateVenueDto {
 
   @IsInt({ message: 'Capacity must be an integer' })
   capacity: number;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Validate((value: any) => {
+    if (!Array.isArray(value)) return false;
+    for (const item of value) {
+      if (!(item instanceof S3ObjectClass)) {
+        return false;
+      }
+    }
+    return true;
+  })
+  images: S3Object[];
 }
